cd51fe23ed2c13bce8cce73a7c9e455b821ea312
Kean Uy <1100015314@edu.cbe.ab.ca>
2021-03-24 13:29:49 -0600

    Commit Mar 24

--- app.js
+++ app.js
@@ -29,3 +29,56 @@ setInterval(() => {
 
 }, 750)
 
+function temporary(){
+    let welcomeSign = [document.createElement("h1"), document.createTextNode("Welcome!")];
+            welcomeSign[0].id = "welcome";
+            
+ 
+            let menuDiv = document.createElement('div');
+            menuDiv.id = "menuDiv";
+ 
+            let inputElem = document.createElement('input');
+            inputElem.id = "fetchInput";
+            inputElem.type = "text";
+            inputElem.placeholder = "Enter API Here";
+ 
+            let listAPI = document.createElement('div');
+            listAPI.id = "listAPI";
+ 
+            let paragraph = [document.createElement('p'), document.createTextNode("https://www.boredapi.com/api/activity")];
+ 
+            let submitButton = document.createElement('button');
+            submitButton.id = "submitButton";
+            submitButton.onclick = () => fetchHandler();
+            let submitText = document.createTextNode("Submit");
+            
+            let errorvar = document.createElement('div');
+            errorvar.id = "errorID";
+ 
+            let space = document.createElement('br');
+            
+            setTimeout(() => {
+                document.body.appendChild(menuDiv);
+                menuDiv.style.animation = "fadeIn 1s";
+ 
+                menuDiv.appendChild(welcomeSign[0]);
+                welcomeSign[0].appendChild(welcomeSign[1]);
+
+                menuDiv.appendChild(inputElem);
+                menuDiv.appendChild(listAPI);
+ 
+                listAPI.appendChild(paragraph[0]);
+                paragraph[0].appendChild(paragraph[1]);
+                
+                menuDiv.appendChild(space);
+ 
+                menuDiv.appendChild(submitButton);
+                submitButton.appendChild(submitText);
+
+                menuDiv.appendChild(space);
+ 
+                menuDiv.appendChild(errorvar);
+            }, 1000);    
+            console.log("Set up complete");
+}
+
