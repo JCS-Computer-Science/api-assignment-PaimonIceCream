97abadeff8b81420370f7c1fad6ec5ea0b184afa
Kean Uy <1100015314@edu.cbe.ab.ca>
2021-04-13 13:29:59 -0600

    initial commitapr 13

--- app.js
+++ app.js
@@ -1,17 +1,99 @@
 function fetchHandler() {
-    console.log("Fetch Start");
-    const buttonVar = document.querySelector(".button");
-    console.log(buttonVar);
-    fetch('https://cat-fact.herokuapp.com/facts/random')
+    var url = document.getElementById("fetchInput").value;
+    
+    fetch(url)  
         .then(response => { 
+            document.getElementById("valueID").innerHTML = " ";
             return response.json();
-        })
+        })   
         .then(response => {
+            let nodeList = document.getElementsByTagName("option").length;
+            for (var i = 0; i < nodeList; i++){
+                let select = document.getElementById("keySelect");
+                select.remove(0);
+            }
+
             
-            console.log(response.text);
-        });
+            let result = Object.keys(response);
+            
+            const dropdown = document.getElementById("keySelect");
+            result.forEach((key) =>  {
+                
+                var option = document.createElement("option");
+
+                option.value = key;
+                option.textContent = key;
+                dropdown.appendChild(option);
+            })
+
+            /* 
+            console.log("test")
+            var key = document.getElementById("dropdown").value
+            document.getElementById("result").innerHTML = result[key]
+            result[key]
+            */
+        }) 
+        .catch(error => {
+            console.log("ERROR");
+            console.error(error);
+            document.getElementById("valueID").innerHTML = "Please submit a valid response.";
+        });        
 }
 
+function finalFetch(){
+    console.log("final Fetch");
+    var url = document.getElementById("fetchInput").value;
+    fetch(url)  
+        .then(response => { 
+            return response.json();
+        })   
+        .then(response => {
+            let result = Object.keys(response);
+            //document.getElementById("valueID").innerHTML = response.activity;
+
+            const dropdown = document.getElementById("keySelect");
+            result.forEach((key) =>  {
+                
+                var option = document.createElement("option");
+
+                option.value = key;
+                option.textContent = key;
+                dropdown.appendChild(option);
+            })
+
+            /* 
+            console.log("test")
+            var key = document.getElementById("dropdown").value
+            document.getElementById("result").innerHTML = result[key]
+            result[key]
+            */
+        }) 
+        .catch(error => {
+            console.log("ERROR");
+            console.error(error);
+            document.getElementById("valueID").innerHTML = "Please submit a valid response.";
+        });
+    
+}
+
+/* Sets up the main menu */
+function setItUp(){
+    const buttonOne = document.getElementById("button")
+    buttonOne.style.transform = "translateY(-100%)";
+    buttonOne.style.animation = "fadeOut 1s";
+    setTimeout(() => {
+        document.getElementById("button").style.display = "none";
+
+        const menudiv = document.getElementById("menuDiv");
+        menudiv.style.display = "inline";
+        menuDiv.style.animation = "fadeIn 1s";
+    }, 900);
+    
+    
+}
+
+
+// Changes the colour of the button
 setInterval(() => {
     var letters = "0123456789ABCDEF";
     var color = '#';
@@ -26,59 +108,5 @@ setInterval(() => {
         color += letters[Math.floor(Math.random() * 16)];
     }
     document.getElementById("button").style.borderColor = color;
-
 }, 750)
-
-function temporary(){
-    let welcomeSign = [document.createElement("h1"), document.createTextNode("Welcome!")];
-            welcomeSign[0].id = "welcome";
-            
- 
-            let menuDiv = document.createElement('div');
-            menuDiv.id = "menuDiv";
- 
-            let inputElem = document.createElement('input');
-            inputElem.id = "fetchInput";
-            inputElem.type = "text";
-            inputElem.placeholder = "Enter API Here";
- 
-            let listAPI = document.createElement('div');
-            listAPI.id = "listAPI";
- 
-            let paragraph = [document.createElement('p'), document.createTextNode("https://www.boredapi.com/api/activity")];
- 
-            let submitButton = document.createElement('button');
-            submitButton.id = "submitButton";
-            submitButton.onclick = () => fetchHandler();
-            let submitText = document.createTextNode("Submit");
-            
-            let errorvar = document.createElement('div');
-            errorvar.id = "errorID";
- 
-            let space = document.createElement('br');
-            
-            setTimeout(() => {
-                document.body.appendChild(menuDiv);
-                menuDiv.style.animation = "fadeIn 1s";
- 
-                menuDiv.appendChild(welcomeSign[0]);
-                welcomeSign[0].appendChild(welcomeSign[1]);
-
-                menuDiv.appendChild(inputElem);
-                menuDiv.appendChild(listAPI);
- 
-                listAPI.appendChild(paragraph[0]);
-                paragraph[0].appendChild(paragraph[1]);
-                
-                menuDiv.appendChild(space);
- 
-                menuDiv.appendChild(submitButton);
-                submitButton.appendChild(submitText);
-
-                menuDiv.appendChild(space);
- 
-                menuDiv.appendChild(errorvar);
-            }, 1000);    
-            console.log("Set up complete");
-}
 
