ada04908ab82b0251db5d9d23dc16b2960a28284
PaimonIceCream <keanu1@educbe.ca>
2021-04-25 22:19:36 -0600

    Final Submission of API Assignment

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Global variables
+/* Global variables */
 var globalJSON;
 var removeBoolean;
 
@@ -28,7 +28,6 @@ function fetchHandler() {
             
             const dropdown = document.getElementById("keySelect");
             result.forEach((key) =>  {
-                
                 let option = document.createElement("option");
                 option.id = key;
                 option.value = key;
@@ -38,19 +37,17 @@ function fetchHandler() {
 
             globalJSON = response;
             removeBoolean = true;
-            
         }) 
         .catch(error => {
             console.log("ERROR");
             console.error(error);
-            document.getElementById("valueID").innerHTML = "Please submit a valid response.";
+            document.getElementById("valueID").innerHTML = "Please Submit a Valid Response";
             let nodeList = document.getElementsByTagName("option").length;
-            
+    
             for (var i = 0; i < nodeList; i++){
                 let select = document.getElementById("keySelect");
                 select.remove(0);
             }
-            console.log("ERROR");
         });        
 }
 
@@ -60,43 +57,77 @@ function fetchChange(){
         select.remove(0);
         removeBoolean = false;
     }
+
     var whichKey = document.getElementById("keySelect").value;
     document.getElementById("valueID").innerHTML = globalJSON[whichKey];
     globalJSON[whichKey]
     console.log(globalJSON);
 }
 
+function addAPI(){
+    let listAPI = document.getElementById("listAPI");
+
+    let storeAPI = [document.createElement("p"), document.createTextNode(document.getElementById("APIadd").value)];
+    storeAPI[0].className = "stored-value";
+    storeAPI[0].ondblclick = () => removeP(event);
+    storeAPI[0].style.color = "rgb(75, 19, 19)";
+
+    listAPI.appendChild(storeAPI[0]);
+    storeAPI[0].appendChild(storeAPI[1]);
+}
+
+function removeP(e){
+    let listAPI = document.getElementById("listAPI");
+    listAPI.removeChild(e.target);
+}
+
+function readMoreToggle(){
+    var b = true;
+
+    if(document.getElementById("menuDiv").style.display == "inline" && b == true){
+        document.getElementById("menuDiv").style.display = "none"
+        document.getElementById("read-more").style.display = "inline"
+        console.log("description screen is present");
+        b = false;
+    }
+
+    if(document.getElementById("menuDiv").style.display = "none" && b == true){
+        document.getElementById("menuDiv").style.display = "inline"
+        document.getElementById("read-more").style.display = "none"
+        console.log("main screen is present");
+        b = false;
+    }
+}
+
 /* Sets up the main menu */
 function setItUp(){
-    const buttonOne = document.getElementById("button")
+    const buttonOne = document.getElementById("mainButton")
     buttonOne.style.transform = "translateY(-100%)";
     buttonOne.style.animation = "fadeOut 1s";
+
     setTimeout(() => {
-        document.getElementById("button").style.display = "none";
+        document.getElementById("mainButton").style.display = "none";
 
         const menudiv = document.getElementById("menuDiv");
         menudiv.style.display = "inline";
         menuDiv.style.animation = "fadeIn 1s";
     }, 900);
-    
-    
 }
 
-
-// Changes the colour of the button
+/* Changes the colour of the button */
 setInterval(() => {
     var letters = "0123456789ABCDEF";
     var color = '#';
     for(let i = 0; i < 6; i++){
         color += letters[Math.floor(Math.random() * 16)];
     }
-    document.getElementById("button").style.backgroundColor = color;
+    document.getElementById("mainButton").style.backgroundColor = color;
 
     letters = "0123456789ABCDEF";
     color = '#';
     for(let i = 0; i < 6; i++){
         color += letters[Math.floor(Math.random() * 16)];
     }
-    document.getElementById("button").style.borderColor = color;
+    document.getElementById("mainButton").style.borderColor = color;
+    document.getElementById("welcome").style.borderColor = color;
 }, 750)
-
